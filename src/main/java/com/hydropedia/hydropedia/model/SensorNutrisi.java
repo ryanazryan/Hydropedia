ed3f57713d6d1d55fea32fb94e57c63290706f25
nadilazari <nadilazari@synapsis.office.id>
2026-06-22 20:38:12 +0700

    fitur sensor tanaman

--- src/main/java/com/hydropedia/hydropedia/model/SensorNutrisi.java
+++ src/main/java/com/hydropedia/hydropedia/model/SensorNutrisi.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -11,3 +12,32 @@ package com.hydropedia.hydropedia.model;
 public class SensorNutrisi {
     
 }
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hydropedia.hydropedia.model;
+
+/**
+ *
+ * @author Ryan
+ */
+public class SensorNutrisi extends Sensor {
+     
+    public SensorNutrisi() { 
+        super(); 
+    }
+    public SensorNutrisi(int idSensor, double nilai) { 
+        super(idSensor, nilai); 
+    }
+    public void bacaNutrisi() {
+        System.out.println("Membaca data dari Sensor Nutrisi...");
+        System.out.println("Kadar nutrisi saat ini: " + getNilai() + " ppm");
+    }
+    @Override
+    public void bacaData() {
+        bacaNutrisi();
+    }
+}
+>>>>>>> Stashed changes
--- src/main/java/com/hydropedia/hydropedia/model/SensorPH.java
+++ src/main/java/com/hydropedia/hydropedia/model/SensorPH.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -11,3 +12,32 @@ package com.hydropedia.hydropedia.model;
 public class SensorPH {
     
 }
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hydropedia.hydropedia.model;
+
+/**
+ *
+ * @author Ryan
+ */
+public class SensorPH extends Sensor {
+  
+    public SensorPH() { 
+        super(); 
+    }
+    public SensorPH(int idSensor, double nilai) { 
+        super(idSensor, nilai); 
+    }
+    public void bacaPH() {
+        System.out.println("Membaca data dari Sensor pH...");
+        System.out.println("Nilai pH saat ini: " + getNilai());
+    }
+    @Override
+    public void bacaData() {
+        bacaPH(); 
+    }
+}
+>>>>>>> Stashed changes
