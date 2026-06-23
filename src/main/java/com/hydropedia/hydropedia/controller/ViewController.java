487e47f2351f193f227bab9d1e961c01356f3abd
ryanazryan <naufalazryan05@gmail.com>
2026-06-23 15:25:22 +0800

    Implement dashboard and thymeleaf UI integration

--- src/main/java/com/hydropedia/hydropedia/controller/ViewController.java
+++ src/main/java/com/hydropedia/hydropedia/controller/ViewController.java
@@ -4,7 +4,12 @@
  */
 package com.hydropedia.hydropedia.controller;
 
+import com.hydropedia.hydropedia.repository.JenisTanamanRepository;
+import com.hydropedia.hydropedia.repository.NotifikasiRepository;
+import com.hydropedia.hydropedia.repository.TanamanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 /**
  *
@@ -13,29 +18,61 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-
+    @Autowired
+    private TanamanRepository tanamanRepository;
+    @Autowired
+    private JenisTanamanRepository jenisTanamanRepository;
+    @Autowired
+    private NotifikasiRepository notifikasiRepository;
     @GetMapping("/")
     public String login() {
         return "login";
     }
-
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        model.addAttribute(
+                "totalTanaman",
+                tanamanRepository.count()
+        );
+        model.addAttribute(
+                "totalJenis",
+                jenisTanamanRepository.count()
+        );
+        model.addAttribute(
+                "totalNotifikasi",
+                notifikasiRepository.count()
+        );
         return "dashboard";
     }
-
     @GetMapping("/tanaman")
-    public String tanaman() {
+    public String tanaman(Model model) {
+
+        model.addAttribute(
+                "tanamanList",
+                tanamanRepository.findAll()
+        );
         return "tanaman";
     }
-
     @GetMapping("/monitoring")
-    public String monitoring() {
+    public String monitoring(Model model) {
+
+        model.addAttribute(
+                "jumlahTanaman",
+                tanamanRepository.count()
+        );
+
+        model.addAttribute(
+                "jumlahNotifikasi",
+                notifikasiRepository.count()
+        );
         return "monitoring";
     }
-
     @GetMapping("/notifikasi")
-    public String notifikasi() {
+    public String notifikasi(Model model) {
+        model.addAttribute(
+                "notifikasiList",
+                notifikasiRepository.findAll()
+        );
         return "notifikasi";
     }
 }
